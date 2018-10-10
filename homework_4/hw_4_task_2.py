b6e5c9ce6affc53c84af2a841ecf5b23128163ae
It academy <itacademy@MacBook-Pro-User.local>
2018-10-10 20:17:44 +0300

    done hw_4, @elpiankova verify plz)/ && git push origin master

---
+++ homework_4/hw_4_task_2.py
@@ -0,0 +1,14 @@
+# Пишем функцию, которая выводит второе по возрастанию значение из переданных аргументов.
+# Учитываем, что может быть повторяющиеся значения аргументов.
+
+
+def premin(*numbers):
+    l = list(numbers)
+    l.sort()
+    min_element = l[0]
+    for element in l:
+        if element > min_element:
+            return element
+
+
+print(premin(2, 2, 3, 4, 5))
